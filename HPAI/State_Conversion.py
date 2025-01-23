bdeb18b4a5c34939360b0b088c83f08581622987
Debbie227 <110264572+Debbie227@users.noreply.github.com>
2025-01-23 09:46:07 -0700

    Create State_Conversion.py
    State conversion function moved from the HPAI-map.py file

---
+++ HPAI/State_Conversion.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+"""
+File name: State_Conversion.py
+Author: Debra Pacheco
+Created: 1/23/25
+Version: 1.0
+Description:
+    This script converts a capitalized US state name to the all upper case State abbreviation
+    Input required: Capitalized US state name with spaces if the state is two words
+
+License: MIT License
+"""
+
+# State abbreviation Dictionary
+state_abbreviation = {
+    # https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States#States.
+    "AK": "Alaska",
+    "AL": "Alabama",
+    "AR": "Arkansas",
+    "AZ": "Arizona",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "HI": "Hawaii",
+    "IA": "Iowa",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "MA": "Massachusetts",
+    "MD": "Maryland",
+    "ME": "Maine",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MO": "Missouri",
+    "MS": "Mississippi",
+    "MT": "Montana",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "NE": "Nebraska",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NV": "Nevada",
+    "NY": "New York",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PA": "Pennsylvania",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VA": "Virginia",
+    "VT": "Vermont",
+    "WA": "Washington",
+    "WI": "Wisconsin",
+    "WV": "West Virginia",
+    "WY": "Wyoming"
+}
+
+# convert a state name to the state abbreviation
+def state_conversion(state):
+    """Converts US State name to abbreviation
+
+        Parameters:
+        argument1 (string): US State name with every word capitalized and spaces between words
+
+        Returns:
+        string:State abbreviation in capital letters
+
+       """
+    for abbreviation, name in state_abbreviation.items():
+        if state == name:
+            return(abbreviation)
+          
