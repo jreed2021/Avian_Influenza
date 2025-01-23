1690de659e4091a50ec2be3354d972ae04942b6b
Debbie227 <110264572+Debbie227@users.noreply.github.com>
2025-01-23 09:44:53 -0700

    Update HPAI-map.py
    Added program description
    Moved state conversion dictionary and function to a new file for readability and code reuse

--- HPAI/HPAI-map.py
+++ HPAI/HPAI-map.py
@@ -1,8 +1,23 @@
+#!/usr/bin/env python3
+
 import requests
 from io import StringIO
 import plotly
 import plotly.express as px
 import pandas as pd
+from State_Conversion import state_conversion
+
+"""
+File name: HPAI-map.py
+Author: Debra Pacheco
+Created: 1/21/25
+Version: 1.2
+Description:
+    This script displays a choropleth map of the United States containing highly pathogenic avian influenza
+    cases obtained from the Aphis USDA website.
+
+License: MIT License
+"""
 
 url = "https://www.aphis.usda.gov/sites/default/files/hpai-mammals.csv"  # HPAI Detections in mammals from USDA
 
@@ -12,70 +27,10 @@ response.raise_for_status()  # Raise an error for bad status codes
 # Read the content directly into a pandas DataFrame
 data = pd.read_csv(StringIO(response.text))
 
-# print(data.head())  # Display the first few rows
+#print(data.tail(30))  # Display the first few rows
 
 # Create a column containing just the year
 data['Year'] = pd.DatetimeIndex(data['Date Detected']).year
-
-state_abbreviation = {
-    # https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States#States.
-    "AK": "Alaska",
-    "AL": "Alabama",
-    "AR": "Arkansas",
-    "AZ": "Arizona",
-    "CA": "California",
-    "CO": "Colorado",
-    "CT": "Connecticut",
-    "DE": "Delaware",
-    "FL": "Florida",
-    "GA": "Georgia",
-    "HI": "Hawaii",
-    "IA": "Iowa",
-    "ID": "Idaho",
-    "IL": "Illinois",
-    "IN": "Indiana",
-    "KS": "Kansas",
-    "KY": "Kentucky",
-    "LA": "Louisiana",
-    "MA": "Massachusetts",
-    "MD": "Maryland",
-    "ME": "Maine",
-    "MI": "Michigan",
-    "MN": "Minnesota",
-    "MO": "Missouri",
-    "MS": "Mississippi",
-    "MT": "Montana",
-    "NC": "North Carolina",
-    "ND": "North Dakota",
-    "NE": "Nebraska",
-    "NH": "New Hampshire",
-    "NJ": "New Jersey",
-    "NM": "New Mexico",
-    "NV": "Nevada",
-    "NY": "New York",
-    "OH": "Ohio",
-    "OK": "Oklahoma",
-    "OR": "Oregon",
-    "PA": "Pennsylvania",
-    "RI": "Rhode Island",
-    "SC": "South Carolina",
-    "SD": "South Dakota",
-    "TN": "Tennessee",
-    "TX": "Texas",
-    "UT": "Utah",
-    "VA": "Virginia",
-    "VT": "Vermont",
-    "WA": "Washington",
-    "WI": "Wisconsin",
-    "WV": "West Virginia",
-    "WY": "Wyoming"
-}
-
-# convert a state name to the state abbreviation
-def state_conversion(state):
-    for abbreviation, name in state_abbreviation.items():
-        if state == name:
-            return(abbreviation)
 
 # Create new column in data with the state abbreviation
 data['Abbreviation'] = data['State'].apply(state_conversion)
